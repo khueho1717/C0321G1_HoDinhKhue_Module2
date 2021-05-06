8725bac204f17ba86cc2ae36f81d209bb4f41420
hyuganejidn@gmail.com <xuan hung>
2021-05-06 11:34:17 +0700

    baitap6

---
+++ BT_TH_Module2/src/bai6_inheritance/bai_tap/circle_circlenin/CircleTest.java
@@ -0,0 +1,8 @@
+package bai6_inheritance.bai_tap.circle_circlenin;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle=new Circle(3,"yellow");
+        System.out.println(circle);
+    }
+}
---
+++ BT_TH_Module2/src/bai6_inheritance/bai_tap/circle_circlenin/CylinderTest.java
@@ -0,0 +1,8 @@
+package bai6_inheritance.bai_tap.circle_circlenin;
+
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder=new Cylinder(3,2,"red");
+        System.out.println(cylinder);
+    }
+}
