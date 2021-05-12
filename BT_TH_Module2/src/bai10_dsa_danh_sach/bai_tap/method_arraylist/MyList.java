5318d8bbeeca9da93b6eedac608b48da4c0e2410
hyuganejidn@gmail.com <xuan hung>
2021-05-12 09:20:21 +0700

    baitap10

--- BT_TH_Module2/src/bai10_dsa_danh_sach/bai_tap/method_arraylist/MyList.java
+++ BT_TH_Module2/src/bai10_dsa_danh_sach/bai_tap/method_arraylist/MyList.java
@@ -15,50 +15,34 @@ public class MyList<E> {
     public MyList(int capacity) {
         elements = new Object[capacity];
     }
+
     private void ensureCapacity(int minCapacity) {
         elements = Arrays.copyOf(elements, minCapacity);
     }
 
     public void add(int index, E element) {
-        if (size == elements.length) {
-            ensureCapacity(1);
-        }
+        size = elements.length;
+        ensureCapacity(size + 1);
         if (index < size) {
             for (int temp = size; index < temp; temp--) {
                 elements[temp] = elements[temp - 1];
             }
             elements[index] = element;
-            size++;
-        } else {
             elements[size++] = element;
         }
     }
 
     public E remove(int index) {
-        Object remove = elements[index];
-        size--;
-        for (; index < size; index++) {
-            elements[index] = elements[index + 1];
-        }
-        return (E) remove;
-    }
-   public void remove4(int index) {
-        int sizeArray = elements.length;
         for (int i = index; i < elements.length - 1; i++) {
             elements[i] = elements[i + 1];
         }
-        sizeArray--;
-        Object removeArray[] = new Object[sizeArray];
-        for (int i = 0; i < sizeArray; i++) {
-            removeArray[i] = elements[i];
-        }
-        for (Object print : removeArray
-        ) {
-            System.out.print(print+"\t");
-        }
+        int sizeArray = elements.length;
+        ensureCapacity(sizeArray--);
+        return (E) elements;
     }
+
     public int getSize() {
-        return size;
+        return elements.length;
     }
 
     public MyList<E> clone() {
@@ -66,148 +50,51 @@ public class MyList<E> {
     }
 
     public boolean contains(E o) {
-        if (size == 0) {
+        int sizeCurent = elements.length;
+        if (sizeCurent == 0) {
             return false;
         } else {
-            for(int i =0;i<size;i++){
-                if(elements[i]==o){
+            for (int i = 0; i < sizeCurent; i++) {
+                if (elements[i] == o) {
                     return true;
                 }
             }
         }
+
         return false;
     }
+
+
     public int indexOf(E o) {
-        for (int i = 0; i < size; i++) {
-            if (o.equals(elements[i])) {
-                return i;
+        int sizeCurent = elements.length;
+        if (sizeCurent == 0) {
+            return -1;
+        } else {
+            for (int i = 0; i < sizeCurent; i++) {
+                if (elements[i] == o) {
+                    return i;
+                }
             }
+            return -1;
         }
-        return -1;
     }
 
     public boolean add(E e) {
+        int sizeCurent=elements.length;
         if (!contains(e)) {
-            if (size == elements.length) {
-                ensureCapacity(1);
-            }
-            elements[size++] = e;
-            return true;
+            ensureCapacity(sizeCurent+1);
+            elements[sizeCurent++]=e;
+            return  true;
         }
         return false;
     }
 
     public E get(int i) {
-        if (i >= size || i < 0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
-        }
         return (E) elements[i];
     }
 
     public void clear() {
         elements = new Object[DEFAULT_CAPACITY];
         size = 0;
-
-//    void add(int index, E element) {
-//        int newSize = elements.length + 1;
-//        elements = Arrays.copyOf(elements, newSize);
-//        for (int i = newSize; i > index; i--) {
-//            if (i == index) {
-//                elements[i] = element;
-//            } else elements[i] = elements[i - 1];
-//            System.out.println(Arrays.toString(elements));
-//        }
-//        for (int i = 0; i < elements.length; i++) {
-//            if (index > elements.length) {
-//                System.out.println("vị trí này không nằm trong mảng");
-//                break;
-//            } else if (i == index) {
-//                elementsNew[i] = element;
-//                break;
-//            } else {
-//                elementsNew[i] = elements[i];
-//            }
-//        }
-//        for (int index2 = index + 1; index2 < elementsNew.length; index2++) {
-//            elementsNew[index2] = elements[index2 - 1];
-//        }
-//        elements=elementsNew;
-//        for (Object print : elements
-//        ) {
-//            System.out.print(print+"\t");
-//        }
-
-
-//    void remove(int index) {
-//        int sizeArray = elements.length;
-//        for (int i = index; i < elements.length - 1; i++) {
-//            elements[i] = elements[i + 1];
-//        }
-//        sizeArray--;
-//        Object removeArray[] = new Object[sizeArray];
-//        for (int i = 0; i < sizeArray; i++) {
-//            removeArray[i] = elements[i];
-//        }
-//        for (Object print : removeArray
-//        ) {
-//            System.out.print(print+"\t");
-//        }
-//    }
-//
-//    public int size() {
-//        return this.size = elements.length;
-//    }
-//
-//    public MyList<E> clone() {
-//        return this;
-//    }
-//
-//    public boolean contains(E o) {
-//        if (elements.length == 0) {
-//            return false;
-//        } else {
-//            for (int i = 0; i < elements.length; i++) {
-//                if (elements[i] == o) {
-//                    return true;
-//                }
-//            }
-//        }
-//
-//        return false;
-//    }
-//    public int indexOf(E o){
-//        if (elements.length==0){
-//            return -1;
-//        }else {
-//            for (int i = 0; i < elements.length; i++) {
-//                if (elements[i] == o) {
-//                    return i;
-//                }
-//            }
-//            return -1;
-//        }
-//    }
-//    public boolean add(E o){
-//        if (!contains(o)){
-//            elements=new Object[size++];
-//            add(size,o);
-//            return true;
-//        }
-//        return false;
-//    }
-//    public void ensureCapacity(int minCapacity) {
-//        elements = Arrays.copyOf(elements, minCapacity);
-//    }
-//    public E get(int i) {
-//        if (i >= size || i < 0) {
-//            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
-//        }
-//        return (E) elements[i];
-//    }
-//
-//    public void clear() {
-//        elements = new Object[DEFAULT_CAPACITY];
-//        size = 0;
-//    }
     }
 }
--- BT_TH_Module2/src/bai10_dsa_danh_sach/bai_tap/method_arraylist/MyListTest.java
+++ BT_TH_Module2/src/bai10_dsa_danh_sach/bai_tap/method_arraylist/MyListTest.java
@@ -7,45 +7,18 @@ public class MyListTest {
         myList.add(1, 2);
         myList.add(2, 3);
         myList.add(3, 4);
-//        System.out.println(myList.get(0));
-//        System.out.println(myList.getSize());
-        myList.add(2, 10);
-//        System.out.println(myList.get(4));
-//        System.out.println(myList.getSize());
-        System.out.println(" ");
-        for (int i = 0; i < myList.getSize(); i++) {
-            System.out.println(myList.get(i));
-        }
-        System.out.println(myList.remove(2));
-//        System.out.println(myList.get(3));
-        System.out.println(" ");
-        for (int i = 0; i < myList.getSize(); i++) {
-            System.out.println(myList.get(i));
-        }
-        System.out.println(myList.contains(7));
-        System.out.println(myList.contains(2));
-        System.out.println(myList.indexOf(6));
-//        myList.clear();
-        myList.add(11);
-//        myList.add(12);
+        myList.remove(1);
+        myList.remove(2);
         System.out.println(myList.getSize());
-        System.out.println(myList.getClass());
+        System.out.println(myList.get(1));
+        System.out.println(myList.contains(3));
+        System.out.println(myList.indexOf(3));
+        System.out.println(myList.add(3));
         MyList<Integer> myClone = myList.clone();
-        System.out.println(" ");
-        for (int i = 0; i < myClone.getSize(); i++) {
-            System.out.println(myClone.get(i));
-        }
-        System.out.println(" ");
-        myClone.clear();
-        myClone.add(1);
-        myClone.add(2);
-        System.out.println(myClone.getSize());
-        System.out.println(myClone.get(1));
-//        myClone.add(2,3);
-//        myClone.add(1,2);
-        System.out.println(myClone.contains(3));
-        System.out.println(myClone.contains(1));
-//        myClone.add(1);
-        System.out.println(myClone.get(0));
+        myList.add(0, 1);
+        myList.add(1, 2);
+        myList.add(2, 3);
+        myList.add(3, 4);
+        System.out.println(myClone.get(2));
     }
 }
